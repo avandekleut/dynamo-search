a1834c08763b036882189fd436b8b047b3b2b31c
Alexander Van de Kleut <54694031+avandekleut@users.noreply.github.com>
2023-02-09 20:13:46 -0500

    init

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,6 +1,6 @@
 import { flattenObject } from '../src/main.js'
 
-describe('greeter function', () => {
+describe('flattenObject', () => {
   beforeAll(async () => {
     return
   })
@@ -28,5 +28,39 @@ describe('greeter function', () => {
     const flattened = flattenObject(objectToFlatten)
     expect(flattened).toHaveProperty(['foo.0'], 'hello')
     expect(flattened).toHaveProperty(['foo.1'], 'world')
+  })
+
+  it('handles edge cases', () => {
+    const obj = {
+      value: {
+        foo: {
+          bar: 'yes',
+          so: {
+            freakin: {
+              nested: 'Wow',
+              date: new Date(),
+            },
+          },
+        },
+      },
+      test: [true, false, [null, undefined, 1]],
+      not_lost: [], // Empty arrays should be preserved
+      not_lost2: {}, // Empty objects should be preserved
+      // Be careful with object having dots in the keys
+      'I.like.dots..in.object.keys...': "... Please don't override me",
+      I: {
+        like: {
+          'dots..in': {
+            object: {
+              'keys...': "You've been overwritten",
+            },
+          },
+        },
+      },
+    }
+    const flattened = flattenObject(obj)
+
+    expect(flattened).toHaveProperty('not_lost')
+    expect(flattened).toHaveProperty('not_lost2')
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,52 @@
-export function flattenObject(object: any, separator = '.'): any {
-  const toReturn = {}
+export function flattenObject(
+  object: Object,
+  prefix: string | undefined = undefined,
+  result = null,
+): Object {
+  result = result || {}
 
-  for (const property in object) {
-    if (!Object.prototype.hasOwnProperty.call(object, property)) {
-      continue
-    }
-
-    if (typeof object[property] == 'object' && object[property] !== null) {
-      const flatObject = flattenObject(object[property])
-
-      for (const subproperty in flatObject) {
-        if (!Object.prototype.hasOwnProperty.call(flatObject, subproperty)) {
-          continue
-        }
-
-        toReturn[property + separator + subproperty] = flatObject[subproperty]
-      }
-    } else {
-      toReturn[property] = object[property]
-    }
+  // Preserve empty objects and arrays, they are lost otherwise
+  if (prefix && isEmpty(object)) {
+    result[prefix] = Array.isArray(object) ? [] : {}
+    return result
   }
 
-  return toReturn
+  prefix = prefix ? prefix + '.' : ''
+
+  for (const i in object) {
+    if (Object.prototype.hasOwnProperty.call(object, i)) {
+      if (isRecursivelyFlattenable(object[i])) {
+        flattenObject(object[i], prefix + i, result)
+      } else {
+        result[prefix + i] = object[i]
+      }
+    }
+  }
+  return result
+}
+
+function isEmpty(object: Object): boolean {
+  if (
+    typeof object === 'object' &&
+    object !== null &&
+    Object.keys(object).length === 0
+  ) {
+    return true
+  }
+  return false
+}
+
+/**
+ * Drops custom classes like Dates, etc
+ */
+function isRecursivelyFlattenable(object: Object): boolean {
+  if (
+    typeof object === 'object' &&
+    (Array.isArray(object) ||
+      Object.prototype.toString.call(object) === '[object Object]') &&
+    object !== null
+  ) {
+    return true
+  }
+  return false
 }
