ccfbc268bc0775308c0ab19eee5e3367b0fa9047
Alexander Van de Kleut <54694031+avandekleut@users.noreply.github.com>
2023-02-10 23:00:30 -0500

    update jest to speed up typescript tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,18 @@
 export default {
   testEnvironment: 'node',
   preset: 'ts-jest/presets/default-esm',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
   transform: {
-    '^.+\\.m?[tj]s?$': ['ts-jest', { useESM: true }],
+    '^.+\\.m?[tj]s?$': [
+      'ts-jest',
+      {
+        /* ts-jest config goes here in Jest */
+      },
+    ],
   },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.(m)?js$': '$1',
