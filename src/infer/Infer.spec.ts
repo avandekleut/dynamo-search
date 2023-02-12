fa41f28bf20bff6ac2be6fb05109ed5c7410df78
Alexander Van de Kleut <54694031+avandekleut@users.noreply.github.com>
2023-02-12 14:58:21 -0500

    all tests passing

--- src/infer/Infer.spec.ts
+++ src/infer/Infer.spec.ts
@@ -4,6 +4,7 @@ import { expect } from '@jest/globals'
 
 import { ArbitraryRepresentation } from './ArbitraryRepresentation'
 import { Infer } from './Infer'
+import { uniquelyInferrableArbitraryFactories } from './__data__'
 import { inferConfig } from './__data__/inferConfig'
 
 const infer = new Infer(inferConfig)
@@ -20,22 +21,18 @@ describe('fixed counterexamples', () => {
   })
 })
 
-describe.skip('pending counterexamples', () => {
-  test('hex', () => {
-    const counterExample = '1000000000'
-    const arbitrary = fc.hexaString()
-    const inferred = infer.infer(counterExample)
-    expect(arbitraryRepresentation.stringify(arbitrary)).toEqual(
-      arbitraryRepresentation.stringify(inferred),
-    )
-  })
-
-  test('ascii', () => {
-    const counterExample = '"++//A++///+/"'
-    const arbitrary = fc.ascii()
-    const inferred = infer.infer(counterExample)
-    expect(arbitraryRepresentation.stringify(arbitrary)).toEqual(
-      arbitraryRepresentation.stringify(inferred),
-    )
+describe('Infer', () => {
+  test('inverts all invertable arbitraries', () => {
+    for (const arbitraryFactory of uniquelyInferrableArbitraryFactories) {
+      const arbitrary = arbitraryFactory()
+      fc.assert(
+        fc.property(arbitrary, (sample) => {
+          const inferred = infer.infer(sample)
+          expect(arbitraryRepresentation.stringify(arbitrary)).toEqual(
+            arbitraryRepresentation.stringify(inferred),
+          )
+        }),
+      )
+    }
   })
 })
